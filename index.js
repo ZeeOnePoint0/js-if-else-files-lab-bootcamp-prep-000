4994a2434678a9ad789bfc797bece7a7b2763913
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-05 17:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var name = "Susan"
 var height = "70"
 
-if (name === "Susan") {
+if (name === "Susan" || height == 70) {
   console.log("The name is correct");
-} else {
+  console.log("The height is correct");
+} else{
   console.log("Expected the name to be Susan");
 }
 
